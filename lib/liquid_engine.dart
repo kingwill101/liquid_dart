08309b5ca52aa92ce8b529dd02ea95e6bf96d072
kingwill101 <hey@glenfordwilliams.com>
2023-12-15 18:07:39 -0500

    export Drop

--- lib/liquid_engine.dart
+++ lib/liquid_engine.dart
@@ -2,5 +2,6 @@ library liquid_engine;
 
 export 'src/block.dart' show Block, SimpleBlockFactory, BlockParser;
 export 'src/context.dart' show Context, Filter, Module, ParseContext, RenderContext;
+export 'src/drop.dart' show Drop;
 export 'src/model.dart' show Source, Root;
 export 'src/template.dart' show Template;
