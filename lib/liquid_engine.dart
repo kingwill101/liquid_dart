c9a82bfeb2a56bf86c78493ef8fd8e714bfd4f8f
kingwill101 <hey@glenfordwilliams.com>
2024-04-18 03:17:14 -0500

    expose more functionality to support custom tags

--- lib/liquid_engine.dart
+++ lib/liquid_engine.dart
@@ -2,6 +2,10 @@ library liquid_engine;
 
 export 'src/block.dart' show Block, SimpleBlockFactory, BlockParser;
 export 'src/context.dart' show Context, Filter, Module, ParseContext, RenderContext;
+export 'src/document.dart' show Document, DocumentFuture, DocumentParser;
 export 'src/drop.dart' show Drop;
 export 'src/model.dart' show Source, Root;
+export 'src/parser/lexer.dart' show Lexer;
+export 'src/parser/parser.dart' show Parser;
+export 'src/parser/tag_parser.dart' show TagParser;
 export 'src/template.dart' show Template;
