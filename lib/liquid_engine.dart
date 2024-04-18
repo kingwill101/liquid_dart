76878ece5515e7dc7edb21ebba5697ba96a9b39b
kingwill101 <hey@glenfordwilliams.com>
2024-04-18 03:22:33 -0500

    expose Token+TokenType

--- lib/liquid_engine.dart
+++ lib/liquid_engine.dart
@@ -4,6 +4,7 @@ export 'src/block.dart' show Block, SimpleBlockFactory, BlockParser;
 export 'src/context.dart' show Context, Filter, Module, ParseContext, RenderContext;
 export 'src/document.dart' show Document, DocumentFuture, DocumentParser;
 export 'src/drop.dart' show Drop;
+export 'src/model.dart' show Source, Root, Token, TokenType;
 export 'src/model.dart' show Source, Root;
 export 'src/parser/lexer.dart' show Lexer;
 export 'src/parser/parser.dart' show Parser;
